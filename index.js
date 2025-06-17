7cad3a5f2c59bc28dbfa2d20fcd490d91662b920
pilot0111 <pilot0111@gmail.com>
2025-06-17 22:44:43 +0300

    Reg Ex added

--- index.js
+++ index.js
@@ -14,10 +14,17 @@ function addURL() {
     name: siteName.value,
     Url: siteUrl.value,
   };
-  bookmarksArr.push(URL);
-  localStorage.setItem("URL", JSON.stringify(bookmarksArr));
-  displayURL();
-  clearInputs();
+  var validResult = isValidURL(siteUrl.value);
+  URL.Url = validResult.url;
+
+  if (validResult.isValid) {
+    bookmarksArr.push(URL);
+    localStorage.setItem("URL", JSON.stringify(bookmarksArr));
+    displayURL();
+    clearInputs();
+  } else {
+    showAlert();
+  }
 }
 
 function displayURL() {
@@ -26,14 +33,12 @@ function displayURL() {
     cartona += `
              <tr>
                 <td>${i}</td>
-                <td>${bookmarksArr[i].name}</td>
-                <td>${bookmarksArr[i].Url}</td>
-              
+                <td>${bookmarksArr[i].name}</td>           
                 <td>
-                  <button class="btn btn-outline-warning" onclick="patchValues(${i})">Visit</button>
+                  <button class="btn btn-visit" onclick="openURL(${i})"><i class="fa-solid fa-eye pe-2"></i> Visit</button>
                 </td>
                 <td>
-                  <button class="btn btn-outline-danger" onclick="deleteProducts(${i})">Delete</button>
+                  <button class="btn btn-delete " onclick="deleteURL(${i})"><i class="fa-solid fa-trash-can"></i> Delete</button>
                 </td>
              </tr>
 `;
@@ -43,31 +48,59 @@ function displayURL() {
   //console.log(productsArr);
 }
 
-localStorage.setItem("userName", "Ahmed");
-var userName = localStorage.getItem("userName");
-if (userName == null) {
-  userName = "Back";
-}
-document.getElementById("welcome").innerHTML = userName;
-
-function deleteProducts(index) {
-  productsArr.splice(index, 1);
-  displayProducts();
-  localStorage.setItem("Product", JSON.stringify(productsArr));
+function deleteURL(index) {
+  bookmarksArr.splice(index, 1);
+  displayURL();
+  localStorage.setItem("URL", JSON.stringify(bookmarksArr));
 }
 
-function patchValues(index) {
-  mainIndex = index;
-  productNameInput.value = productsArr[index].name;
-  productPriceInput.value = productsArr[index].price;
-  productCategoryInput.value = productsArr[index].category;
-  productDescInput.value = productsArr[index].description;
-  addUpdateProduct.innerHTML = "Update Product";
+function openURL(index) {
+  var url = bookmarksArr[index].Url;
+  if (url) {
+    window.open(url, "_blank"); // Opens in a new tab
+  } else {
+    console.error("URL not found at index", index);
+  }
 }
 
 function clearInputs() {
-  productNameInput.value = "";
-  productPriceInput.value = "";
-  productCategoryInput.value = "";
-  productDescInput.value = "";
+  siteName.value = "";
+  siteUrl.value = "";
+}
+
+function isValidURL(url) {
+  if (!/^https?:\/\//i.test(url)) {
+    if (/^www\./i.test(url)) {
+      url = `https://${url}`;
+    } else {
+      url = `https://www.${url}`;
+    }
+  }
+//   if (/^[a-zA-Z0-9]{3,}\.[a-zA-Z]{2,5}$/.test(url)) {
+//     url = `https://www.${url}`;
+//   } else if (/^(www\.)[a-zA-Z0-9-]{3,}\.[a-zA-Z]{2,5}$/.test(url)) {
+//     url = `https://${url}`;
+//   }else if(/^(https:\/\/)(?!www\.).*[a-zA-Z0-9-]{3,}\.[a-zA-Z]{2,5}$/.test(url)){
+// url = `https://www.${url.slice(8)}`;
+//   }else if(/^(http:\/\/)(?!www\.).*[a-zA-Z0-9-]{3,}\.[a-zA-Z]{2,5}$/.test(url)){
+// url = `https://www.${url.slice(7)}`;
+//   }
+  var pattern = /^(https?:\/\/)?(www\.)?[a-zA-Z0-9-]{3,}\.[a-zA-Z]{2,5}$/;
+  var isValid = pattern.test(url);
+console.log(url)
+  return { isValid, 
+      url };
+}
+
+function showAlert() {
+  Swal.fire({
+    icon: "error",
+    title: "Site Name or URL is not valid",
+    html: `
+      <p class="text-danger-emphasis fw-bold fs-4 py-2">Please follow the rules below:</p>
+      <p class="text-bg-danger text-start">➤ Site name must contain at least 3 characters</p>
+     <p class="text-bg-danger text-start">➤ Site URL must be a valid one</p>
+      
+    `,
+  });
 }
